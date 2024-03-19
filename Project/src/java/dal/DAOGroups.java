eaed73ebb0d4a647e3ab3f88f6c861583dcc87c6
Hoàng Đức <dieuthanki123@gmail.com>
2024-03-19 17:58:46 +0700

    ok
    thêm chức năng chấm điểm

--- Project/src/java/dal/DAOGroups.java
+++ Project/src/java/dal/DAOGroups.java
@@ -19,10 +19,10 @@ import model.Student;
  *
  * @author Admin
  */
-public class DAOGroups extends DBContext{
-    public ArrayList<Course> loadCourse()
-    {
-        int out=0; // 
+public class DAOGroups extends DBContext {
+
+    public ArrayList<Course> loadCourse() {
+        int out = 0; // 
         ArrayList<Group> group = new ArrayList<>();
         ArrayList<Course> courses = new ArrayList<>();
         int Code_cr;
@@ -32,49 +32,46 @@ public class DAOGroups extends DBContext{
                 + "FROM Courses INNER JOIN\n"
                 + " Groups ON Courses.Code = Groups.CodeCourse";
         try {
-            PreparedStatement st= connection.prepareStatement(sql);
-            ResultSet rs=st.executeQuery();
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
             rs.next();
-                Code_cr = rs.getInt("Code");
-                Name_cr = rs.getString("Name");
-                sign_cr = rs.getString("Sign");
-            while(true)
-            {
-                while(rs.getInt("Code") == Code_cr)
-                {
+            Code_cr = rs.getInt("Code");
+            Name_cr = rs.getString("Name");
+            sign_cr = rs.getString("Sign");
+            while (true) {
+                while (rs.getInt("Code") == Code_cr) {
                     Group gr = new Group(rs.getInt("CodeGroup"), rs.getString("NameGroup"));
                     group.add(gr);
-                    if(!rs.next())
-                    {
-                        out=1;
+                    if (!rs.next()) {
+                        out = 1;
                         break;
                     }
                 }
-                courses.add(new Course(Code_cr,Name_cr,sign_cr,group));
+                courses.add(new Course(Code_cr, Name_cr, sign_cr, group));
                 group = new ArrayList<>();
-                
-                if(out == 0){
-                Code_cr = rs.getInt("Code");
-                Name_cr = rs.getString("Name");
-                sign_cr = rs.getString("Sign");
-                Group gr = new Group(rs.getInt("CodeGroup"), rs.getString("NameGroup"));
-                group.add(gr);
+
+                if (out == 0) {
+                    Code_cr = rs.getInt("Code");
+                    Name_cr = rs.getString("Name");
+                    sign_cr = rs.getString("Sign");
+                    Group gr = new Group(rs.getInt("CodeGroup"), rs.getString("NameGroup"));
+                    group.add(gr);
                 }
-                if(!rs.next())
+                if (!rs.next()) {
                     break;
+                }
             }
-        } catch (SQLException  | NumberFormatException e) {
+        } catch (SQLException | NumberFormatException e) {
             System.out.println("when accessing the database \"Course\" got an error");
         }
         return courses;
     }
-    
-    public ArrayList<Department> loadDepartment()
-    {
+
+    public ArrayList<Department> loadDepartment() {
         int out = 0;
         ArrayList<Department> department = new ArrayList<>();
         ArrayList<Course> courses = new ArrayList<>();
-        
+
         int Code_dp;
         String Name_dp; // dp = Department
         String sql = "SELECT Departments.Code, Departments.Name, Courses.Code AS CodeCourse, Courses.Name AS NameCourse, Courses.[Sign] AS SignCourse\n"
@@ -82,48 +79,45 @@ public class DAOGroups extends DBContext{
                 + " Courses ON Departments.Code = Courses.CodeDP\n"
                 + "order by Code asc";
         try {
-            PreparedStatement st= connection.prepareStatement(sql);
-            ResultSet rs=st.executeQuery();
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
             rs.next();
-                Code_dp = rs.getInt("Code");
-                Name_dp = rs.getString("Name");
-            while(true)
-            {
-                while(rs.getInt("Code") == Code_dp)
-                {
-                    
-                    Course cr = new Course(rs.getInt("CodeCourse"), rs.getString("NameCourse"),rs.getString("SignCourse"));
+            Code_dp = rs.getInt("Code");
+            Name_dp = rs.getString("Name");
+            while (true) {
+                while (rs.getInt("Code") == Code_dp) {
+
+                    Course cr = new Course(rs.getInt("CodeCourse"), rs.getString("NameCourse"), rs.getString("SignCourse"));
                     courses.add(cr);
-                    
-                    if(!rs.next())
-                    {
-                        out=1;
+
+                    if (!rs.next()) {
+                        out = 1;
                         break;
                     }
                 }
-                department.add(new Department(Code_dp,Name_dp,courses));
+                department.add(new Department(Code_dp, Name_dp, courses));
                 courses = new ArrayList<>();
-                if(out == 0){
-                Code_dp = rs.getInt("Code");
-                Name_dp = rs.getString("Name");
-                Course cr = new Course(rs.getInt("CodeCourse"), rs.getString("NameCourse"),rs.getString("SignCourse"));
-                courses.add(cr);
+                if (out == 0) {
+                    Code_dp = rs.getInt("Code");
+                    Name_dp = rs.getString("Name");
+                    Course cr = new Course(rs.getInt("CodeCourse"), rs.getString("NameCourse"), rs.getString("SignCourse"));
+                    courses.add(cr);
                 }
-                if(!rs.next()) 
+                if (!rs.next()) {
                     break;
+                }
             }
-        } catch (SQLException  | NumberFormatException e) {
+        } catch (SQLException | NumberFormatException e) {
             System.out.println("when accessing the database \"Department\" got an error");
         }
         return department;
     }
-    
-    public ArrayList<Group> loadGroups()
-    {
+
+    public ArrayList<Group> loadGroups() {
         int out = 0;
         ArrayList<Group> gr = new ArrayList<>();
         ArrayList<Student> std = new ArrayList<>();
-        
+
         int Code_gr;
         String Name_gr; // gr = group std = student
         String sql = "SELECT        Groups.Code AS ClassID,Groups.Name AS Class, InforStudents.Code, InforStudents.Name AS Name\n"
@@ -132,42 +126,39 @@ public class DAOGroups extends DBContext{
                 + "                         InforStudents ON Students.Code = InforStudents.Code\n"
                 + "ORDER BY ClassID";
         try {
-            PreparedStatement st= connection.prepareStatement(sql);
-            ResultSet rs=st.executeQuery();
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
             rs.next();
-                Code_gr = rs.getInt("ClassID");
-                Name_gr = rs.getString("Class");
-            while(true)
-            {
-                while(rs.getInt("ClassID") == Code_gr)
-                {
+            Code_gr = rs.getInt("ClassID");
+            Name_gr = rs.getString("Class");
+            while (true) {
+                while (rs.getInt("ClassID") == Code_gr) {
                     std.add(new Student(rs.getString("Name"), rs.getString("Code")));
-                    
-                    if(!rs.next())
-                    {
-                        out=1;
+
+                    if (!rs.next()) {
+                        out = 1;
                         break;
                     }
-                    
+
                 }
-                gr.add(new Group(Code_gr,Name_gr,std));
+                gr.add(new Group(Code_gr, Name_gr, std));
                 std = new ArrayList<>();
-                if(out == 0){
-                Code_gr = rs.getInt("ClassID");
-                Name_gr = rs.getString("Class");
-                std.add(new Student(rs.getString("Name"), rs.getString("Code")));
+                if (out == 0) {
+                    Code_gr = rs.getInt("ClassID");
+                    Name_gr = rs.getString("Class");
+                    std.add(new Student(rs.getString("Name"), rs.getString("Code")));
                 }
-                if(!rs.next()) 
+                if (!rs.next()) {
                     break;
+                }
             }
-        } catch (SQLException  | NumberFormatException e) {
+        } catch (SQLException | NumberFormatException e) {
             System.out.println("when accessing the database \"Groups\" got an error");
         }
         return gr;
     }
-    
-    public ArrayList<Student> loadStudents(int Code)
-    {
+
+    public ArrayList<Student> loadStudents(int Code) {
         ArrayList<Student> std = new ArrayList();
         int Code_gr;
         String Name_gr; // gr = group std = student
@@ -175,30 +166,46 @@ public class DAOGroups extends DBContext{
                 + "FROM            Groups INNER JOIN\n"
                 + "                         Students ON Groups.Code = Students.CodeGroup INNER JOIN\n"
                 + "                         InforStudents ON Students.Code = InforStudents.Code\n"
-                +"where Groups.Code = ? ";
+                + "where Groups.Code = ? ";
         try {
-            PreparedStatement st= connection.prepareStatement(sql);
+            PreparedStatement st = connection.prepareStatement(sql);
             st.setInt(1, Code);
-            ResultSet rs=st.executeQuery();
-            while(rs.next())
-            {
-                
-                    std.add(new Student(rs.getString("Name"), rs.getString("Code")));
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+
+                std.add(new Student(rs.getString("Name"), rs.getString("Code")));
             }
-        } catch (SQLException  | NumberFormatException e) {
+        } catch (SQLException | NumberFormatException e) {
             System.out.println("when accessing the database \"Student\" got an error");
         }
         return std;
     }
+
+    public int CourseOfGroups(int Code) {
+        String sql = "SELECT        Groups.Code as CodeGroup, Courses.Code AS CodeCourse\n"
+                + "FROM            Courses INNER JOIN\n"
+                + "                         Groups ON Courses.Code = Groups.CodeCourse\n"
+                + "Where Groups.Code =?";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, Code);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                return rs.getInt("CodeCourse");
+            }
+        } catch (SQLException | NumberFormatException e) {
+            System.out.println("when accessing the database \"Course Of Group\" got an error");
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        DAOGroups object= new DAOGroups();
+        DAOGroups object = new DAOGroups();
         ArrayList<Group> crs = object.loadGroups();
-        for(Group i : crs)
-        {
-            System.out.println(i.getName()+" "+i.getCode()+" ");
-            for(Student j : i.getStudent())
-            {
-                System.out.println("       "+j.getMSSV()+" "+j.getName());
+        for (Group i : crs) {
+            System.out.println(i.getName() + " " + i.getCode() + " ");
+            for (Student j : i.getStudent()) {
+                System.out.println("       " + j.getMSSV() + " " + j.getName());
             }
         }
     }
